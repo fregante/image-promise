34b7939187d75d72f5c46e7f5a32c482d8bca917
Federico Brigante <bfred-it@users.noreply.github.com>
2016-07-23 21:30:45 +0700

    Code golfing

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 'use strict';
 
-const EMPTY_IMAGE = 'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=';
-
-function load(src) {
+export default function load(src) {
 	// if more than one argument, treat as
 	// load('1.jpg', '2.jpg')
-	if (typeof arguments[1] === 'string') {
-		src = Array.apply(null, arguments);
+	if (arguments.length > 1) {
+		src = Array.apply(0, arguments);
 	}
 
 	// if first argument is an array, treat as
@@ -15,11 +13,11 @@ function load(src) {
 		return Promise.all(src.map(load));
 	}
 
-	const image = new Image(); // putting this outside the condition avoids an IIFE in babel
+	const image = new Image();
 	if (!load[src]) {
 		load[src] = new Promise((resolve, reject) => {
-			image.addEventListener('load', resolve.bind(null, image));
-			image.addEventListener('error', reject.bind(null, image));
+			image.addEventListener('load', () => resolve(image));
+			image.addEventListener('error', () => reject(image));
 			image.src = src;
 
 			if (image.complete) {
@@ -34,8 +32,8 @@ function load(src) {
 load.unload = function (src) {
 	// if more than one argument, treat as
 	// load('1.jpg', '2.jpg')
-	if (typeof arguments[1] === 'string') {
-		src = Array.apply(null, arguments);
+	if (arguments.length > 1) {
+		src = Array.apply(0, arguments);
 	}
 
 	// if first argument is an array, treat as
@@ -44,14 +42,13 @@ load.unload = function (src) {
 		return Promise.all(src.map(load.unload));
 	}
 
+	const unload = image => {
+		// GC, http://www.fngtps.com/2010/mobile-safari-image-resource-limit-workaround/
+		image.src = 'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=';
+		delete load[src];
+	};
 	if (load[src]) {
-		return load[src].catch(() => {}).then(image => {
-			// GC, http://www.fngtps.com/2010/mobile-safari-image-resource-limit-workaround/
-			image.src = EMPTY_IMAGE;
-			delete load[src];
-		});
+		return load[src].then(unload, unload);
 	}
 	return Promise.resolve();
 };
-
-export default load;
