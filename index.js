4051fb0075b7c2b858033a9385c323a705aa571b
Federico Brigante <bfred-it@users.noreply.github.com>
2017-02-28 23:46:47 +0700

    Handle invalid input

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 export default function load(image) {
-	if (typeof image === 'string') {
+	if (!image) {
+		return Promise.reject(image);
+	} else if (typeof image === 'string') {
 		// If image is a string, "convert" it to an <img>
 		const src = image;
 		image = new Image();
@@ -8,6 +10,9 @@ export default function load(image) {
 		// If image is Array-like, treat as
 		// load(['1.jpg', '2.jpg'])
 		return Promise.all([].map.call(image, load));
+	} else if (image.tagName !== 'IMG') {
+		// If it's not an <img> tag, reject
+		return Promise.reject(image);
 	}
 
 	const promise = new Promise((resolve, reject) => {
