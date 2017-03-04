66226041571f3481090b661720f6f7b5add93ef6
Federico Brigante <bfred-it@users.noreply.github.com>
2017-03-04 13:39:53 +0700

    Remove intermediate test launcher
    Travis’ tests can be launched explicitly. The current launcher had
    issues: no errors output

--- tests/launch.js
+++
@@ -1,9 +0,0 @@
-var isTravis = require('is-travis');
-var npmRunAll = require('npm-run-all');
-
-npmRunAll('intern:' + (isTravis ? 'browserstack' : 'chrome'), {
-	stdout: process.stdout,
-	stdout: process.stderr
-}).catch((result) => {
-	process.exit(1);
-});
