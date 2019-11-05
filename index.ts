f87609ebe0eca0dde0da79d8fc5434182bd64f28
Federico Brigante <opensource@bfred.it>
2019-11-05 21:18:46 +0700

    Convert to TypeScript

---
+++ index.ts
@@ -0,0 +1,79 @@
+type Input = string | HTMLImageElement;
+type Output = HTMLImageElement;
+type Attributes = Record<string, string>;
+
+type ImagePromise = Promise<HTMLImageElement> & {
+	image: HTMLImageElement;
+};
+
+function isArrayLike(input: any): input is ArrayLike<Input> {
+	return input.length !== undefined;
+}
+
+function loadSingleImage(image: HTMLImageElement): ImagePromise {
+	const promise = new Promise<HTMLImageElement>((resolve, reject) => {
+		if (image.naturalWidth) {
+			// If the browser can determine the naturalWidth the image is already loaded successfully
+			resolve(image);
+		} else if (image.complete) {
+			// If the image is complete but the naturalWidth is 0px it is probably broken
+			reject(image);
+		} else {
+			image.addEventListener('load', fulfill);
+			image.addEventListener('error', fulfill);
+		}
+
+		function fulfill(): void {
+			if (image.naturalWidth) {
+				resolve(image);
+			} else {
+				reject(image);
+			}
+
+			image.removeEventListener('load', fulfill);
+			image.removeEventListener('error', fulfill);
+		}
+	});
+
+	return Object.assign(promise, {image});
+}
+
+function loadImages(input: Input, attributes?: Attributes): Promise<Output>;
+function loadImages(input: ArrayLike<Input>, attributes?: Attributes): Promise<Output[]>;
+function loadImages(input: Input | ArrayLike<Input>, attributes: Attributes = {}): Promise<Output | Output[]> {
+	if (input instanceof HTMLImageElement) {
+		return loadSingleImage(input);
+	}
+
+	if (typeof input === 'string') {
+		/* Create a <img> from a string */
+		const src = input;
+		const image = new Image();
+		Object.keys(attributes).forEach(
+			name => image.setAttribute(name, attributes[name])
+		);
+		image.src = src;
+		return loadSingleImage(image);
+	}
+
+	if (isArrayLike(input)) {
+		// Momentarily ignore errors
+		const reflect = (img: Input): Promise<HTMLImageElement | Error> => loadImages(img, attributes).catch((error: Error) => error);
+		const reflected = [].map.call(input, reflect) as Array<HTMLImageElement | Error>;
+		return Promise.all(reflected).then((results: Array<HTMLImageElement | Error>) => {
+			const loaded = results.filter((x: any): x is HTMLImageElement => x.naturalWidth);
+			if (loaded.length === results.length) {
+				return loaded;
+			}
+
+			return Promise.reject({
+				loaded,
+				errored: results.filter((x: any): x is Error => !x.naturalWidth)
+			});
+		});
+	}
+
+	return Promise.reject(new TypeError('input is not an image, a URL string, or an array of them.'));
+}
+
+export default loadImages;
